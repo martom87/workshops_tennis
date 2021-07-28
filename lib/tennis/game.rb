3351bcd85dde4cf2c3c6c8d9f3f74af120cfa1c6
Marcin Tomala <marcintomala87@gmail.com>
2021-07-28 15:23:26 +0200

    Working situation after deuce

--- lib/tennis/game.rb
+++ lib/tennis/game.rb
@@ -1,14 +1,13 @@
 require_relative 'player'
 require 'pry'
+
 class Game
   def initialize(players = [])
     @players = players
   end
 
   def call
-   find_winner
-
-   # find_winner_after_deuce
+    find_winner
   end
 
   private
@@ -16,31 +15,26 @@ class Game
   attr_accessor :players
 
   def find_winner
-    unless (players.first.points_amount == players.last.points_amount) || (players.first.points_amount < 40 && players.second.points_amount < 40)
-      winner = players.max_by { |player| player.points_amount }
-      "The winner is #{winner.name}" if winner.points_amount == 40
-    else
-      announce_deuce
-    end
+    winner = players.max_by { |player| player.points_amount }
+    return "The winner is #{winner.name}" if ((players.first.points_amount < 40) && (players.last.points_amount < 40))
+    announce_deuce
   end
 
   def announce_deuce
-    if players.first.points_amount == 40
-      'Deduce'
-    end
+    return 'Deduce' if players.first.points_amount == 40 && players.last.points_amount == 40
+    find_winner_after_deuce
   end
 
   def find_winner_after_deuce
     player1 = players.first.points_amount
     player2 = players.last.points_amount
 
-    if (player1 > 40) && (player2>40)
     if player1 - player2 == 2
-      "The winner is #{players.first.name}"
-    else
-      "The winner is #{players.last.name}"
+      winner = "The winner is #{players.first.name}"
     end
+    if player2 - player1 == 2
+      winner = "The winner is #{players.last.name}"
     end
-
+    winner
   end
 end
--- spec/tennis/game_spec.rb
+++ spec/tennis/game_spec.rb
@@ -5,7 +5,7 @@ require_relative '../../lib/tennis/game.rb'
 RSpec.describe Game do
 
   subject { described_class.new(players).call }
-  let(:player1) { Player.new(40, 'Karolina') }
+  let(:player1) { Player.new(39, 'Karolina') }
   let(:player2) { Player.new(15, 'Marcin') }
   let(:players) { [player1, player2] }
 
@@ -25,12 +25,20 @@ RSpec.describe Game do
         expect(subject).to eq('Deduce')
       end
     end
-
+    #
     context 'When player1 wins in the deuce' do
       let(:player1) { Player.new(42, 'Karolina') }
       let(:player2) { Player.new(40, 'Marcin') }
       it 'indicates player1 as the winner' do
         expect(subject).to eq("The winner is #{player1.name}")
+      end
+    end
+
+    context 'When there was deuce but there is still no winner' do
+      let(:player1) { Player.new(41, 'Karolina') }
+      let(:player2) { Player.new(40, 'Marcin') }
+      it 'indicates player1 as the winner' do
+        expect(subject).to eq(nil)
       end
     end
 
