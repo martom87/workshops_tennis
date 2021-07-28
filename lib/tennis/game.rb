60732dc71c06ef1b47258e42f98664e176fee9d7
Marcin Tomala <marcintomala87@gmail.com>
2021-07-28 11:28:09 +0200

    Adds first rule to game

--- lib/tennis/game.rb
+++ lib/tennis/game.rb
@@ -1,3 +1,19 @@
+require_relative 'player'
 class Game
+  def initialize(players=[])
+    @players = players
+  end
 
+  def call
+    "The winner is #{find_winner.name}"
+  end
+
+  private
+
+  attr_reader :players
+
+
+  def find_winner
+    players.max_by{|player| player.points_amount}
+  end
 end
--- spec/tennis/game_spec.rb
+++ spec/tennis/game_spec.rb
@@ -4,4 +4,19 @@ require_relative '../../lib/tennis/game.rb'
 
 RSpec.describe Game do
 
+  subject { described_class.new(players).call }
+  let(:player1) { Player.new(40, 'Karolina') }
+  let(:player2) { Player.new(15, 'Marcin') }
+  let(:players) { [player1, player2] }
+
+  describe '#call' do
+    context 'When player1 one has more points wins' do
+      let(:winner) { "The winner is #{player1.name}" }
+
+      it 'indicates player one as the winner ' do
+        expect(subject).to eq(winner)
+      end
+    end
+
+  end
 end
